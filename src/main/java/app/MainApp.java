1d52dd4db3287fc376aa8f539ed1457b3485187e
epu_baal_laptop <herdin86@gmail.com>
2020-04-29 00:20:05 +0900

    remove test code in main

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -7,15 +7,9 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
-//@MapperScan(value = "app.mapper")
 public class MainApp {
     public static void main(String[] args) {
-//        SpringApplication application = new SpringApplication(MainApp.class);
-//        application.run(args);
-        try {
-            System.out.println(InetAddress.getLocalHost().toString());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
+        SpringApplication application = new SpringApplication(MainApp.class);
+        application.run(args);
     }
 }
