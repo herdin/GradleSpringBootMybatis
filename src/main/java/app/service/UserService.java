d6cf1960bfae6a0cc079dee9bd52fe9ab5d88a23
epu_baal_laptop <herdin86@gmail.com>
2020-04-29 15:45:07 +0900

    add @lazy to UserService

--- src/main/java/app/service/UserService.java
+++ src/main/java/app/service/UserService.java
@@ -3,10 +3,12 @@ package app.service;
 import app.dto.request.UserReqeust;
 import app.mapper.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Lazy
     @Autowired
     TestMapper testMapper;
 
