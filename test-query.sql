bc16ac05e549e96cc13d8add3042f20542160180
epu_baal_laptop <herdin86@gmail.com>
2020-05-28 15:46:34 +0900

    update

--- test-query.sql
+++ test-query.sql
@@ -6,7 +6,7 @@ select * from test;
 --
 insert into tb_card
 select
-  '1040121031182730'
+  '1010001731089502'
   , card_sta_cd
   , card_prd_id
   , 'herdin'
@@ -16,8 +16,13 @@ from tb_card where 1=1
 and card_no = '1010000100010001'
 
 ;
-select * from tb_card where 1=1
---and card_no = '1010010131588568'
-and card_sta_cd = '00'
+select * from tb_card tc where 1=1
+--and tc.card_no = '1010010131588568'
+and tc.rgsr_id = 'herdin'
+and tc.card_sta_cd = '00'
+and not exists (
+    select 1 from tb_memcard tm where 1=1
+    and tc.card_no = tm.card_no
+)
 ;
 select * from tb_memcard
